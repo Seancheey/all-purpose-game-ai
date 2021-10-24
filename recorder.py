d5038a13e75bbd3f49c30c04dabbd3d11a543d50
Qiyi Shan <adls371@outlook.com>
2021-10-24 17:38:03 -0400

    Add FPS and time elapse bar. Increase fps

--- recorder.py
+++ recorder.py
@@ -12,6 +12,7 @@ from screeninfo import screeninfo
 from typing import List, Tuple, Set
 from ratelimit import rate_limited, sleep_and_retry
 from data_format import keys, img_size
+from rich.progress import Progress, TextColumn, TimeElapsedColumn
 
 
 @dataclass
@@ -36,22 +37,24 @@ class DatasetItem:
 @dataclass
 class Recorder:
     save_dir: str
-    fps: int = 60
+    fps: int = 20
     screen_res: Tuple[int] = img_size
     recording_keys: Set[str] = field(default_factory=lambda: keys.copy())
     exit_key: str = 'q'
 
     def record(self):
-        start_time = datetime.now()
+        folder_name = datetime.now().strftime('%Y%m%d-%H%M%S')
+        os.mkdir(os.path.join(self.save_dir, folder_name))
         stop_event = Event()
+
         with ThreadPoolExecutor(3) as pool:
-            screen_future = pool.submit(self.__record_screen, stop_event)
+            screen_future = pool.submit(self.__record_screen, stop_event, folder_name)
             keyboard_future = pool.submit(self.__record_keyboard, stop_event)
             pool.submit(self.__listen_to_stop_event, stop_event).result()
 
             screen_data = screen_future.result()
             keyboard_data = keyboard_future.result()
-        self.__save_records(keyboard_data, screen_data, start_time.strftime('%Y%m%d-%H%M%S'))
+        self.__save_records(keyboard_data, screen_data, folder_name)
 
     def __listen_to_stop_event(self, stop_event):
         keyboard.wait(self.exit_key)
@@ -70,7 +73,7 @@ class Recorder:
         stop_event.wait()
         return key_sequence
 
-    def __record_screen(self, stop_event: Event) -> List[ScreenEvent]:
+    def __record_screen(self, stop_event: Event, folder: str) -> List[ScreenEvent]:
         sct = mss()
         monitors = screeninfo.get_monitors()
         assert len(monitors) > 0, OSError('No Monitor Detected.')
@@ -85,24 +88,36 @@ class Recorder:
             'height': monitor.height
         }
 
+        video_writer = cv2.VideoWriter(os.path.join(self.save_dir, folder, f'video.avi'),
+                                       cv2.VideoWriter_fourcc(*"XVID"), self.fps, self.screen_res)
+
         @sleep_and_retry
         @rate_limited(1, 1 / self.fps)
         def capture():
             # noinspection PyTypeChecker
             return np.array(sct.grab(bounding_box))[::down_sample_factor, ::down_sample_factor]
 
+        last_timestamp = datetime.now().timestamp()
         screens = []
 
-        while not stop_event.is_set():
-            img = capture()
-            cv2.imshow('screen', img)
-            screens.append(ScreenEvent(img, datetime.now().timestamp()))
-            cv2.waitKey(1)
+        with Progress(
+                TimeElapsedColumn(),
+                TextColumn("[progress.description]{task.fields[fps]}")
+        ) as progress:
+            task_id = progress.add_task('recording', fps=0)
+            while not stop_event.is_set():
+                img = capture()
+                video_writer.write(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+                timestamp = datetime.now().timestamp()
+                screens.append(ScreenEvent(img, timestamp))
+                fps = 1 / (timestamp - last_timestamp)
+                last_timestamp = timestamp
+                progress.update(task_id, fps=f'{round(fps, 1)} frame per sec')
 
-        cv2.destroyAllWindows()
+        video_writer.release()
         return screens
 
-    def __save_records(self, key_sequence: List[KeyEvent], screen_sequence: List[ScreenEvent], filename_suffix):
+    def __save_records(self, key_sequence: List[KeyEvent], screen_sequence: List[ScreenEvent], folder: str):
         ki, si = 0, 0
         cur_keys = set()
         data_out = []
@@ -121,9 +136,9 @@ class Recorder:
                     key_codes=list(cur_keys)
                 ))
                 si += 1
-        np.save(os.path.join(self.save_dir, f'screen-{filename_suffix}'),
+        np.save(os.path.join(self.save_dir, folder, f'screen'),
                 np.array(list(map(lambda x: x.screen, data_out))))
-        np.save(os.path.join(self.save_dir, f'key-{filename_suffix}'),
+        np.save(os.path.join(self.save_dir, folder, f'key'),
                 np.array(list(map(lambda x: x.key_codes, data_out)), dtype=object))
 
 
