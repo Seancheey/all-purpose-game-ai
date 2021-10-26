649065d7be8637f6d589bfe7832f2f5b9193f1be
Qiyi Shan <adls371@outlook.com>
2021-10-26 02:38:55 -0400

    Add random NN model

---
+++ model.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn
+from data_format import img_size, recording_keys
+
+
+class ANN(nn.Module):
+    def __init__(self):
+        super(ANN, self).__init__()
+        self.flatten = nn.Flatten()
+        self.ann_stack = nn.Sequential(
+            nn.Linear(img_size[0] * img_size[1] * 3, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 16),
+            nn.ReLU(),
+            nn.Linear(16, len(recording_keys)),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = torch.div(x, 255)
+        x = self.flatten(x)
+        return self.ann_stack(x)
+
+
+if __name__ == '__main__':
+    model = ANN().to('cuda')
+    test = torch.rand(1, 192, 108, 3, device='cuda')
+    model(test)
