19b5d7146c0a01494f9bec12de6bc92ab28e5b64
Qiyi Shan <adls371@outlook.com>
2021-10-29 04:00:32 -0400

    add oversampler

--- helper/data_loader.py
+++ helper/data_loader.py
@@ -5,10 +5,12 @@ from torch.utils.data import Dataset
 from torch.utils.data.dataset import T_co
 from helper.data_format import np_keys_filename, np_screens_filename, img_size, key_map
 import numpy as np
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler
 
 
 class LineaDataset(Dataset):
-    def __init__(self, data_dir, train=True, batch_size=64, device='cpu'):
+    def __init__(self, data_dir, train=True, batch_size=64, device='cpu', seed=0):
         self.batch_size = batch_size
         record_dirs = list(map(lambda path: os.path.join(data_dir, path), os.listdir(data_dir)))
         keys_list = [np.load(os.path.join(record_dir, np_keys_filename)) for record_dir in record_dirs]
@@ -18,13 +20,16 @@ class LineaDataset(Dataset):
         assert len(self.screens) == len(self.keys), "screen size and key size is not matching"
 
         # split to train/test dataset
-        train_dataset_cut_index = round(0.9 * len(self.screens))
         if train:
-            self.screens = self.screens[:train_dataset_cut_index]
-            self.keys = self.keys[:train_dataset_cut_index]
+            self.screens, _, self.keys, _ = train_test_split(self.screens, self.keys, random_state=seed)
         else:
-            self.screens = self.screens[train_dataset_cut_index:]
-            self.keys = self.keys[train_dataset_cut_index:]
+            _, self.screens, _, self.keys = train_test_split(self.screens, self.keys, random_state=seed)
+
+        # bootstrap to resample minority data
+        sm = RandomOverSampler(random_state=seed)
+        self.screens = self.screens.reshape((len(self.screens), -1))
+        self.screens, self.keys = sm.fit_resample(self.screens, self.keys)
+        self.screens = self.screens.reshape((-1, img_size[1], img_size[0], 3))
 
         # random shuffle
         rng = np.random.default_rng()
