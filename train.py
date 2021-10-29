6d3c136d8ed083fcf39f646dcb3e52fa38667c05
Qiyi Shan <adls371@outlook.com>
2021-10-28 22:57:20 -0400

    add stop button

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 import os
 
+import keyboard
 import torch
 from torch import nn
+
 from helper.data_loader import LineaDataset
 from helper.model import ANN
 from rich.progress import Progress, TextColumn, TimeElapsedColumn
+from threading import Event
 
 learning_rate = 1e-3
 batch_size = 200
@@ -12,6 +15,7 @@ epochs = 500
 device = 'cuda'
 
 if __name__ == '__main__':
+    print('press q to stop training')
     data_dir = os.path.join(os.getcwd(), 'data')
     train_dataset = LineaDataset(data_dir, train=True, batch_size=batch_size, device=device)
     test_dataset = LineaDataset(data_dir, train=False, batch_size=batch_size, device=device)
@@ -19,6 +23,8 @@ if __name__ == '__main__':
     model = ANN().to(device)
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    stop_event = Event()
+    keyboard.add_hotkey('q', lambda: stop_event.set())
 
     with Progress(
             TimeElapsedColumn(),
@@ -27,6 +33,8 @@ if __name__ == '__main__':
     ) as progress:
         task = progress.add_task('', loss='None', epoch=0)
         for epoch in range(epochs):
+            if stop_event.is_set():
+                break
             for X, y in train_dataset:
                 pred = model(X)
                 loss = loss_fn(pred, y)
