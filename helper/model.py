142495f32fdc0dea626872549b6888b9f92a16a9
Qiyi Shan <adls371@outlook.com>
2021-10-30 13:53:34 -0400

    Use CNN for model

--- helper/model.py
+++ helper/model.py
@@ -8,16 +8,25 @@ from helper.data_format import key_map
 class ANN(nn.Module):
     def __init__(self):
         super(ANN, self).__init__()
+        self.cnn_stack = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=(8, 8), stride=(2, 2), padding=2),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.Conv2d(16, 32, kernel_size=(3, 3), padding=1),
+            nn.MaxPool2d(kernel_size=(2, 2))
+        )
         self.ann_stack = nn.Sequential(
-            nn.Linear(62208, 1024),
+            nn.Linear(9568, 1024),
+            nn.Dropout(p=0.3),
             nn.ReLU(),
             nn.Linear(1024, 1024),
+            nn.Dropout(p=0.3),
             nn.Sigmoid(),
             nn.Linear(1024, key_map.shape[0]),
             nn.Sigmoid()
         )
 
     def forward(self, x: torch.Tensor):
+        x = self.cnn_stack(x)
         x = torch.flatten(x, start_dim=1)
         x = self.ann_stack(x)
         return x
@@ -25,6 +34,6 @@ class ANN(nn.Module):
 
 if __name__ == '__main__':
     model = ANN().to('cuda')
-    summary(model, (1, 3, 108, 192))
+    summary(model, (3, 108, 192), batch_size=64)
     test = torch.rand(100, 3, 108, 192, device='cuda')
     print(model(test))
