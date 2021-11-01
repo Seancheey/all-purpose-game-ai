1e02786a2f8d0f97e548be53275e87534d5c87b3
Qiyi Shan <adls371@outlook.com>
2021-11-01 02:28:01 -0400

    add fps to ai applier

--- helper/game_ai_applier.py
+++ helper/game_ai_applier.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 from threading import Event
 
 import keyboard
@@ -26,21 +27,26 @@ class GameAiApplier:
         keyboard.wait(self.start_apply_hotkey)
         print(f'Press {self.stop_apply_hotkey} to stop.')
 
-        with Progress('Prediction: keys: {task.fields[keys]}') as progress:
-            task = progress.add_task('', keys=[])
-            cur_keys = set()
-            for img in self.screen_streamer.stream(self.__stop_event):
-                img = image_to_tensor(img)
-                pred = self.trained_model(torch.reshape(img, (1,) + img.shape))[0]
-                keys = set(self.key_transformer.pred_to_keys(pred))
-                to_press = keys - cur_keys
-                to_release = cur_keys - keys
-                for key in to_press:
-                    keyboard.press(key)
-                for key in to_release:
-                    keyboard.release(key)
-                cur_keys = keys
-                progress.update(task, keys=keys)
+        last_timestamp = datetime.now().timestamp()
+        with Progress('Prediction: fps: {task.fields[fps]} keys: {task.fields[keys]}') as progress:
+            with torch.no_grad():
+                task = progress.add_task('', fps=None, keys=[])
+                cur_keys = set()
+                for img in self.screen_streamer.stream(self.__stop_event):
+                    img = image_to_tensor(img)
+                    pred = self.trained_model(torch.reshape(img, (1,) + img.shape))[0]
+                    keys = set(self.key_transformer.pred_to_keys(pred))
+                    to_press = keys - cur_keys
+                    to_release = cur_keys - keys
+                    for key in to_press:
+                        keyboard.press(key)
+                    for key in to_release:
+                        keyboard.release(key)
+                    cur_keys = keys
+                    timestamp = datetime.now().timestamp()
+                    fps = round(1 / (timestamp - last_timestamp), 1)
+                    last_timestamp = timestamp
+                    progress.update(task, fps=fps, keys=keys)
 
             for key in cur_keys:
                 keyboard.release(key)
