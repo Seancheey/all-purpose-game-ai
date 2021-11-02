cd875220d282483cfb55b14475e0048392dbdb91
Qiyi Shan <adls371@outlook.com>
2021-11-02 00:17:33 -0400

    add error handling and scale method

--- helper/window_region.py
+++ helper/window_region.py
@@ -24,9 +24,22 @@ class WindowRegion:
         if platform.system() != 'Windows':
             raise NotImplementedError("Systems other than Windows are not support for getting window region by name.")
         import win32gui
+        import pywintypes
         handle = win32gui.FindWindow(None, window_title)
-        rect = win32gui.GetWindowRect(handle)
+        try:
+            rect = win32gui.GetWindowRect(handle)
+        except pywintypes.error:
+            rect = None
+        if rect is None:
+            raise OSError(f"Unable to find the window with title \"{window_title}\"")
         return WindowRegion(x=rect[0], y=rect[1], width=rect[2] - rect[0], height=rect[3] - rect[1])
+
+    def scale(self, ratio: float):
+        new_width = round(self.width * ratio)
+        new_height = round(self.height * ratio)
+        new_x = self.x - new_width // 2
+        new_y = self.y - new_height // 2
+        return WindowRegion(x=new_x, y=new_y, width=new_width, height=new_height)
 
     def to_mss_bounding_box(self) -> Dict[str, int]:
         return {
